db2c727edbf0af5795334c19cc11b9ab04120e41
Charan <58394919+charanpy@users.noreply.github.com>
2022-01-08 14:41:02 +0530

    limited post

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [process.env.CLIENT,'http://localhost:3000'],
+    origin: process.env.CLIENT,
   })
 );
 cloudinary();
